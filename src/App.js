fced7fc20b0edf0f0e273828bbb8e1e39e159987
Omri Ram <omriram08@gmail.com>
2020-07-01 20:37:16 +0300

    changing background

--- src/App.js
+++ src/App.js
@@ -95,6 +95,7 @@ class App extends Component {
           padding: "1rem",
           border: "solid 5px #dad7d7",
           borderRadius: "10px",
+          backgroundColor: "red",
         }}
       >
         <Row
