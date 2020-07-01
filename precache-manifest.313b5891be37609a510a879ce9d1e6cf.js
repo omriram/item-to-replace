3a920ebe4003bfbd2529223f8a639af00497f027
Omri Ram <omriram08@gmail.com>
2020-07-01 19:50:39 +0300

    Updates

---
+++ precache-manifest.313b5891be37609a510a879ce9d1e6cf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b98c2462b33bb1db4d82d41988545ea1",
+    "url": "/item-to-replace/index.html"
+  },
+  {
+    "revision": "044c43724a3818806d5d",
+    "url": "/item-to-replace/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "11aae1e9b3f8b3a6d37e",
+    "url": "/item-to-replace/static/css/main.ea519aec.chunk.css"
+  },
+  {
+    "revision": "044c43724a3818806d5d",
+    "url": "/item-to-replace/static/js/2.90d9667a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/item-to-replace/static/js/2.90d9667a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11aae1e9b3f8b3a6d37e",
+    "url": "/item-to-replace/static/js/main.77aef877.chunk.js"
+  },
+  {
+    "revision": "cdc1fc07ab4daec46b41",
+    "url": "/item-to-replace/static/js/runtime-main.9b11940b.js"
+  },
+  {
+    "revision": "95c9c62d38dad252d50293370021f3f2",
+    "url": "/item-to-replace/static/media/logo.95c9c62d.jpg"
+  }
+]);
